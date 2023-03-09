baa29b175827ebaa47ba865c6b0d9159396d344d
k8jang <58891323+k8jang@users.noreply.github.com>
2023-03-08 22:33:14 -0800

    Initial Commit

---
+++ Puzzle V2/Puzzle_V2App.swift
@@ -0,0 +1,17 @@
+//
+//  Puzzle_V2App.swift
+//  Puzzle V2
+//
+//  Created by Kate Jang on 3/8/23.
+//
+
+import SwiftUI
+
+@main
+struct Puzzle_V2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
